d7574ad17461861b88f5d1f1c06960172a2f4aee
devthejo <jo@surikat.pro>
2023-06-21 08:33:14 +0200

    fix: up yarn-lock-to-package-json

--- sources/lib/expand-lock.js
+++ sources/lib/expand-lock.js
@@ -1,6 +1,4 @@
-// const yarnLockToPackageJson = require('yarn-lock-to-package-json')
-//temp re-add local fork until https://github.com/rohit-gohri/yarn-lock-to-package-json/pull/8 is merged
-const yarnLockToPackageJson = require('./yarn-lock-to-package-json')
+const yarnLockToPackageJson = require('yarn-lock-to-package-json')
 
 module.exports = (cmd)=>{
   cmd.context.stdout.write("[YARN-FETCH] extracting package.json file(s) from yarn.lock\n")
